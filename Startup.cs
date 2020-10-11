c736d0d1fe985c8c511d17447f04d41f70ff5178
twtseng <twtseng@users.noreply.github.com>
2020-10-11 16:06:21 -0700

    no redirection

--- Startup.cs
+++ Startup.cs
@@ -60,7 +60,7 @@ namespace TheWall
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
